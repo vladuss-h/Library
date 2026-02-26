83eb0c1299fc425611df473507e2874fc2a6c692
vladuss-h <vladimira.husarcikova@gmail.com>
2026-02-26 18:31:21 +0100

    Small changes

--- Library.App/Models/Loan.cs
+++ Library.App/Models/Loan.cs
@@ -2,13 +2,13 @@
 {
     public class Loan : BaseEntity
     {
-        public int BookId { get; set; }
+        public int BookId { get; set; }//FK
         public Book Book { get; set; }
 
-        public int ReaderId { get; set; }
+        public int ReaderId { get; set; }//FK
         public Reader Reader { get; set; }
 
         public DateTime BorrowedAt { get; set; }
-        public DateTime? ReturnedAt { get; set; }
+        public DateTime? ReturnedAt { get; set; } // nullable, protože kniha môže byť stále požičaná (aktívna výpožička = ReturnDate je null)
     }
 }
